64e347f346cd3f9f2bab02c92da2a38f6ca39ad1
Galen Church <galenechurch@gmail.com>
2024-09-18 17:00:08 -0400

    CCSI write from char device

--- imx-ssi.c
+++ imx-ssi.c
@@ -96,6 +96,11 @@ static int ccsidev_write(struct file *file, const char __user *u_buf, size_t siz
         return -EFAULT;
     }
 
+    ///zero out the bus and transition from start
+    writel(0xFFFF, g_ssi->base + REG_SSI_STX0); //pull high
+    udelay(10);
+    writel(0xFFFE, g_ssi->base + REG_SSI_STX0); //trasnition low one CC before data
+
     int i = 0;
     while(i < size){
         if (i+1 < size){
